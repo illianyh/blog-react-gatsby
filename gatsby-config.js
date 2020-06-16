b2f0256f65b4b01979c2af71aef9d1b8f0a286c4
Illiany Hamdan <illianyhamdan@gmail.com>
2020-06-16 17:51:40 +0100

    Add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`limelight`, `source sans pro\:300,400,400i,700`],
+        fonts: [`roboto mono`, `muli\:400,400i,700, 700i`],
         display: "swap",
       },
     },
