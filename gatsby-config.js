cca0a4a59bd4cd5c53ed92785b2e62b465cdac1f
Illiany Hamdan <illianyhamdan@gmail.com>
2020-06-16 16:45:22 +0100

    Add google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,5 +37,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`limelight`, `source sans pro\:300,400,400i,700`],
+        display: "swap",
+      },
+    },
   ],
 }
